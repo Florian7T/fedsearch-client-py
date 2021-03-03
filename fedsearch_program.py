19e7a22c5c060a8f38a2e9a28590047a54d6a5e1
Florian <68027868+Florian7T@users.noreply.github.com>
2021-03-03 09:03:04 +0100

    Update fedsearch_program.py
    Removed the need for fedsearch.py

--- fedsearch_program.py
+++ fedsearch_program.py
@@ -1,26 +1,36 @@
-import os,fedsearch
+import os,requests
 
-api = fedsearch.api()
 if os.name == 'nt':
     os.system('cls')
 else:
     os.system('clear')
 
 def check_if_any(x:str)-> bool:
-    if x.find('{email}') !=-1 or x.find('{password}') !=-1 or x.find('{email}') !=-1 or x.find('{token}') !=-1 or x.find('{ip}') !=-1 or x.find('{database}') !=-1:
+    if x.find('{email}') !=-1 or x.find('{password}') !=-1 or x.find('{email}') !=-1 or x.find('{token}') !=-1 or x.find('{ip}') !=-1  or x.find('{database}') !=-1 or x.find('{hash}') !=-1 or x.find('{num}') !=-1:
         return True
     return False
 
-print("Welcome to fedsearch.cf!")
+def search(key1, query): # Stolen (skid moment)
+    r = requests.post('https://fedsearch.cf/API/search_api.php', data={'search': query, 'submit': '', 'key': key1,}).json()
+    if 'DOCTYPE' in r:
+        return f'\033[31m[Error]\033[0m Cloudflare Ratelimit'
+    elif 'Blocked' in r:
+        return f'\033[31m[Error]\033[0m Blocked By Cloudflare'
+    elif 'Dirrect' in r:
+        return f'\033[31m[Error]\033[0m Dirrect Access Error'
+    else:
+        return r
+
+print("Welcome to fedsearch! #cba adding colors i\'ll do that later or something ")
 key = input("Key => ")
-if len(api.search(key,'monkey')) == 2: # cba reading & filtering the html
+if len(search(key,'monkey')) == 2: # cba reading & filtering the html
     print("Invalid key! The program will now exit..")
     exit(0)
 else:
     print("Valid key!")
 
 out_format = input("""
-Format options: {email}, {password}, {ip}, {token}, {database}, {num}
+Format options: {email}, {password} or {hash}, {ip}, {token}, {database}, {num}
 Format examples:
 # {email} {password}
 # {email}+{password} | {ip} {database}
@@ -33,14 +43,14 @@ if not check_if_any(out_format):
     exit(0)
 
 while True:
-    response = api.search(key, input("Query => "))
+    response = search(key, input("Query => "))
     c=0
     print()
     for result in response:
         c+=1
         try:
-            email, password, ip, token, database = result ['email'], result ['password'], result ['ip'], result ['token'], result ['database'] # I know I know..
-            print(out_format.replace('{email}',email).replace('{password}',password).replace('{ip}',ip).replace('{token}',token).replace('{database}',database).replace('{num}',str(c)))
+            email, password, ip, token, database = result ['email'], result ['password'], result ['ip'], result ['token'], result ['database']
+            print(out_format.replace('{email}',email).replace('{password}',password).replace('{ip}',ip).replace('{token}',token).replace('{database}',database).replace('{num}',str(c)).replace('{hash}',password))
         except TypeError:
             print("# 0 results for this Query")
             break
