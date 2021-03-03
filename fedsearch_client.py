1fc24f673e5bb29c538502cf35f58f6c4bce5189
Florian <68027868+Florian7T@users.noreply.github.com>
2021-03-03 19:56:01 +0100

    Update fedsearch_client.py
    more code cleaning

--- fedsearch_client.py
+++ fedsearch_client.py
@@ -15,15 +15,16 @@ else:
     print("Valid key!")
 
 out_format = input("""
-Format options: {email}, {password} or {hash}, {ip}, {token}, {database}, {count}
+Format options: 
 Format examples:
 # {email} {password}
 # {email}+{password} | {ip} {database}
 # {count}: {email} ___ {hash}
 
 Format => """)
-
-if out_format.find('{email}') ==-1 and out_format.find('{password}') ==-1 and out_format.find('{email}') ==-1 and out_format.find('{token}') ==-1 and out_format.find('{ip}') ==-1  and out_format.find('{database}') ==-1 and out_format.find('{hash}') ==-1 and out_format.find('{count}') ==-1:
+#thanks a lot man. I usually code in java I've only recently started using python for smaller things like this
+format_options = ['{email}', '{password}', '{hash}', '{ip}', '{token}', '{database}', '{count}']
+if not any(x in out_format for x in format_options):
     print("Non valid Format: Make sure to use at least 1 of the options")
     print("The program will now exit..")
     exit(0)
@@ -31,26 +32,31 @@ if out_format.find('{email}') ==-1 and out_format.find('{password}') ==-1 and ou
 
 while True:
     out = []
-    response = requests.post('https://fedsearch.cf/API/search_api.php',data={'search': input("Query => "), 'submit': '', 'key': key, }).json()
+    query = input("Query => ")
+    response = requests.post('https://fedsearch.cf/API/search_api.php',data={'search': query, 'submit': '', 'key': key, }).json()
 
     print()
     c = 0
     if 'No Results Found' in str(response):
-        print('No Results Found!\n')
+        print(f'No Results Found! Query: {query}\n')
         continue
 
     for result in response:
         c += 1
-        email, password, ip, token, database = result ['email'], result ['password'], result ['ip'], result ['token'], result ['database']
-        opp = out_format.replace('{email}', email).replace('{password}', password).replace('{ip}', ip).replace('{token}', token).replace('{database}', database).replace('{count}', str(c)).replace('{hash}',password)
+        email, password, ip, token, database = result['email'], result['password'], result['ip'], result['token'], result['database']
+        opp = out_format.replace('{email}', email)\
+                        .replace('{password}', password)\
+                        .replace('{ip}', ip)\
+                        .replace('{token}', token)\
+                        .replace('{database}', database)\
+                        .replace('{count}', str(c))\
+                        .replace('{hash}',password)
         print(opp)
         out.append(opp)
     print()
     print("Writing to logfile..")
-    fn = str(datetime.datetime.now().strftime("%H_%M_%S-%d_%m_%y"))+'.txt'
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),fn),'w+') as f:
-        for out_ in out:
-            f.write(out_.replace('\ufffd','')+"\n")
-        f.close()
+    fn = datetime.datetime.now().strftime("%H_%M_%S-%d_%m_%y")+'.txt'
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),fn),'w') as f:
+        f.write('\n'.join(out).replace('\ufffd','') + '\n')
     print(f"Output saved to: \'{fn}\'")
     print()
