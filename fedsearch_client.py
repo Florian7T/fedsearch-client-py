318654f5893d3115d2f4324312c65e48b2027646
Florian <68027868+Florian7T@users.noreply.github.com>
2021-03-03 18:56:06 +0100

    Update fedsearch_client.py
    cleaned the code

--- fedsearch_client.py
+++ fedsearch_client.py
@@ -5,25 +5,10 @@ if os.name == 'nt':
 else:
     os.system('clear')
 
-def check_if_any(x:str)-> bool:
-    if x.find('{email}') !=-1 or x.find('{password}') !=-1 or x.find('{email}') !=-1 or x.find('{token}') !=-1 or x.find('{ip}') !=-1  or x.find('{database}') !=-1 or x.find('{hash}') !=-1 or x.find('{count}') !=-1:
-        return True
-    return False
 
-def search(key1, query): # Stolen (skid moment)
-    r = requests.post('https://fedsearch.cf/API/search_api.php', data={'search': query, 'submit': '', 'key': key1,}).json()
-    if 'DOCTYPE' in r:
-        return f'\033[31m[Error]\033[0m Cloudflare Ratelimit'
-    elif 'Blocked' in r:
-        return f'\033[31m[Error]\033[0m Blocked By Cloudflare'
-    elif 'Dirrect' in r:
-        return f'\033[31m[Error]\033[0m Dirrect Access Error'
-    else:
-        return r
-
-print("Welcome to fedsearch! #cba adding colors i\'ll do that later or something ")
+print("Welcome to fedsearch!")
 key = input("Key => ")
-if len(search(key,'monkey')) == 2: # cba reading & filtering the html
+if len(requests.post('https://fedsearch.cf/API/search_api.php',data={'search': 'monkey', 'submit': '', 'key': key, }).json()) == 2: # using this until proper method is available
     print("Invalid key! The program will now exit..")
     exit(0)
 else:
@@ -38,36 +23,34 @@ Format examples:
 
 Format => """)
 
-if not check_if_any(out_format):
+if out_format.find('{email}') ==-1 and out_format.find('{password}') ==-1 and out_format.find('{email}') ==-1 and out_format.find('{token}') ==-1 and out_format.find('{ip}') ==-1  and out_format.find('{database}') ==-1 and out_format.find('{hash}') ==-1 and out_format.find('{count}') ==-1:
     print("Non valid Format: Make sure to use at least 1 of the options")
     print("The program will now exit..")
     exit(0)
 
-out = []
-while True:
-    response = search(key, input("Query => "))
 
-    c = 0
+while True:
+    out = []
+    response = requests.post('https://fedsearch.cf/API/search_api.php',data={'search': input("Query => "), 'submit': '', 'key': key, }).json()
+
     print()
+    c = 0
+    if 'No Results Found' in str(response):
+        print('No Results Found!\n')
+        continue
+
     for result in response:
         c += 1
-        try:
-            email, password, ip, token, database = result ['email'], result ['password'], result ['ip'], result ['token'], result ['database']
-            o = out_format.replace('{email}', email).replace('{password}', password).replace('{ip}', ip).replace('{token}', token).replace('{database}', database).replace('{count}', str(c)).replace('{hash}',password)
-            print(o)
-            out.append(o)
-        except TypeError:
-            print("# 0 results for this Query")
-            break
+        email, password, ip, token, database = result ['email'], result ['password'], result ['ip'], result ['token'], result ['database']
+        opp = out_format.replace('{email}', email).replace('{password}', password).replace('{ip}', ip).replace('{token}', token).replace('{database}', database).replace('{count}', str(c)).replace('{hash}',password)
+        print(opp)
+        out.append(opp)
     print()
     print("Writing to logfile..")
-    fn = str(datetime.datetime.now()).replace(' ','_').replace(':','_')
-    fn = fn[:fn.find('.')]+'.txt'
-    with open(os.path.dirname(os.path.realpath(__file__))+'\\'+fn,'w+') as f:
-        for x in out:
-            f.write(x.replace('\ufffd','')+"\n")
-        f.flush()
+    fn = str(datetime.datetime.now().strftime("%S_%M_%H-%d_%m_%y"))+'.txt'
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),fn),'w+') as f:
+        for out_ in out:
+            f.write(out_.replace('\ufffd','')+"\n")
         f.close()
     print(f"Output saved to: \'{fn}\'")
     print()
-
